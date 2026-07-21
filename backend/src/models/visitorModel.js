0b4790f65f61472a94ca3259c2db79f00217794f
RestuWibisonoo <restu.wibisonoo@gmail.com>
2026-07-21 23:31:45 +0700

    fix: mysql2 promise syntax in visitorModel

--- backend/src/models/visitorModel.js
+++ backend/src/models/visitorModel.js
@@ -1,21 +1,21 @@
 const db = require('../config/db');
 
 const visitorModel = {
-  recordVisit: (ipAddress) => {
-    return new Promise((resolve, reject) => {
+  recordVisit: async (ipAddress) => {
+    try {
       const query = `
         INSERT IGNORE INTO visitor_logs (ip_address, visit_date) 
         VALUES (?, CURDATE())
       `;
-      db.query(query, [ipAddress], (err, results) => {
-        if (err) reject(err);
-        resolve(results);
-      });
-    });
+      const [results] = await db.query(query, [ipAddress]);
+      return results;
+    } catch (error) {
+      throw error;
+    }
   },
 
-  getStats: () => {
-    return new Promise((resolve, reject) => {
+  getStats: async () => {
+    try {
       const query = `
         SELECT 
           (SELECT COUNT(*) FROM visitor_logs WHERE visit_date = CURDATE()) AS hari_ini,
@@ -23,17 +23,19 @@ const visitorModel = {
           (SELECT COUNT(*) FROM visitor_logs WHERE YEAR(visit_date) = YEAR(CURDATE())) AS tahun_ini,
           (SELECT COUNT(*) FROM visitor_logs WHERE YEAR(visit_date) = YEAR(CURDATE()) - 1) AS tahun_kemarin
       `;
-      db.query(query, (err, results) => {
-        if (err) reject(err);
-        resolve(results[0] || {
-          hari_ini: 0,
-          bulan_ini: 0,
-          tahun_ini: 0,
-          tahun_kemarin: 0
-        });
-      });
-    });
+      const [results] = await db.query(query);
+      return results[0] || {
+        hari_ini: 0,
+        bulan_ini: 0,
+        tahun_ini: 0,
+        tahun_kemarin: 0
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 };
+
+module.exports = visitorModel;
 
 module.exports = visitorModel;
